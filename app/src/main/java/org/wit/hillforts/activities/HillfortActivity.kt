1cdc447ce45f01f1edcd31d1056ba36ba77a624c
Jeremy Leahy <jeremyleahy77@gmail.com>
2020-10-17 19:12:41 +0100

    Hillforts Model package added

--- app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
@@ -6,18 +6,22 @@ import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
+import org.wit.hillfort.models.HillfortModel
 import org.wit.hillforts.R
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
+
+  var hillfort = HillfortModel()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     info("Hillfort Activity started..")
     setContentView(R.layout.activity_hillfort)
 
     btnAdd.setOnClickListener() {
-      val hillfortTitle = hillfortTitle.text.toString()
-      if (hillfortTitle.isNotEmpty()) {
-        info("add Button Pressed: $hillfortTitle")
+      hillfort.title = hillfortTitle.text.toString()
+      if (hillfort.title.isNotEmpty()) {
+        info("add Button Pressed: $hillfort")
       }
       else {
         toast ("Please Enter a title")
