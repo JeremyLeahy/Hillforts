0ebd33cfa1b520132d4253f345f96ebe67e9192a
Jeremy Leahy <jeremyleahy77@gmail.com>
2020-10-17 18:28:21 +0100

    Event handler added

--- app/src/main/java/org/wit/hillforts/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillforts/HillfortActivity.kt
@@ -2,13 +2,25 @@ package org.wit.hillforts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     info("Hillfort Activity started..")
     setContentView(R.layout.activity_hillfort)
+
+    btnAdd.setOnClickListener() {
+      val hillfortTitle = hillfortTitle.text.toString()
+      if (hillfortTitle.isNotEmpty()) {
+        info("add Button Pressed: $hillfortTitle")
+      }
+      else {
+        toast ("Please Enter a title")
+      }
+    }
   }
 }
