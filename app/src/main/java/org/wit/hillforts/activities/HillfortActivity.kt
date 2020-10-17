b0443a628e4cf0f760de7b76b629ecd97e246069
Jeremy Leahy <jeremyleahy77@gmail.com>
2020-10-17 18:42:56 +0100

    layouts extended to include toolbar, scrollview and a linear layout
    new string resources added to include hint and button addHillfort
    
    button added to layout
    
    Event Handler added
    
    application structure refactored: new package org.wit.hillforts.activities created

--- app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
@@ -1,4 +1,4 @@
-package org.wit.hillforts
+package org.wit.hillforts.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,6 +6,7 @@ import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
+import org.wit.hillforts.R
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
   override fun onCreate(savedInstanceState: Bundle?) {
