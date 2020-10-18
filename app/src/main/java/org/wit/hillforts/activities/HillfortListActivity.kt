d0f9f186f4576374d4264ae3304ac0996e689b10
Jeremy Leahy <jeremyleahy77@gmail.com>
2020-10-18 20:24:17 +0100

    Action bar added and implemented on home screen

--- app/src/main/java/org/wit/hillforts/activities/HillfortListActivity.kt
+++ app/src/main/java/org/wit/hillforts/activities/HillfortListActivity.kt
@@ -1,14 +1,13 @@
 package org.wit.hillforts.activities
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_hillfort_list.*
 import kotlinx.android.synthetic.main.card_hillfort.view.*
+import org.jetbrains.anko.startActivityForResult
 import org.wit.hillfort.main.MainApp
 import org.wit.hillfort.models.HillfortModel
 import org.wit.hillforts.R
@@ -22,9 +21,24 @@ class HillfortListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_hillfort_list)
         app = application as MainApp
 
+        toolbar.title = title
+        setSupportActionBar(toolbar)
+
         val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = HillfortAdapter(app.hillforts)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.item_add -> startActivityForResult<HillfortActivity>(0)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
 
