634f9fa573fc1c6985e86f5570d8a0949428e063
jeremyleahy <Tregender@10>
2020-10-19 18:32:35 +0100

    Model update to include id's as a way of identifying hillforts

--- app/src/main/java/org/wit/hillfort/models/HillfortModel.kt
+++ app/src/main/java/org/wit/hillfort/models/HillfortModel.kt
@@ -5,5 +5,6 @@ import kotlinx.android.parcel.Parcelize
 
 
 @Parcelize
-data class HillfortModel(var title: String = "",
-                          var description: String = "") : Parcelable
+data class HillfortModel(var id: Long = 0,
+                         var title: String = "",
+                         var description: String = "") : Parcelable
